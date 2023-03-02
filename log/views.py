841074a12e4a76fa7d77e013d53b6d722354888f
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-02 18:35:28 +0600

    auto commit

--- log/views.py
+++ log/views.py
@@ -23,15 +23,16 @@ def log(request):
     categories = collect_categories(current_user)
 
     context = {
-        'user'          : current_user.username,
-        'categories'    : categories,
-        #'entries'       : entries,
+        'user': current_user.username,
+        'categories': categories,
+        # 'entries'       : entries,
     }
-    
+
     # if it doesnt work just load a page with only user context
     # and make a request from another view to load all else
     # and also write this another view like
     return render(request, 'log/log.html', context)
+
 
 @login_required
 def load_content(request):
@@ -41,8 +42,6 @@ def load_content(request):
 
     # Send back JSON
     return JsonResponse(entries_dict)
-
-
 
 
 @csrf_exempt
@@ -56,7 +55,8 @@ def add(request):
     category = Category.objects.get(name=category)
     # user = User.objects.get
 
-    entry = Entry(user=request.user, value=float(value), category=category, comment=comment, date=now())
+    entry = Entry(user=request.user, value=float(value),
+                  category=category, comment=comment, date=now())
     entry.save()
 
     # Get the list of entries -> transform it to the dictionary for jsonifying
@@ -65,13 +65,14 @@ def add(request):
     # Send back JSON
     return JsonResponse(entries_dict)
 
+
 @csrf_exempt
 @login_required
 def remove(request, p):
     ''' Remove entry #p (p stands for position)'''
     # Calculate the id from given position in the list
     entries_list = collect_entries(request.user)
-    
+
     # Take entry with position p
     entry = entries_list[p]
     # Retrieve id from this dictionary object
@@ -80,7 +81,6 @@ def remove(request, p):
     entry_to_delete = Entry.objects.get(id=id)
     # Finally - delete it
     entry_to_delete.delete()
-    
 
     # Reload updated list of entries -> transform it to the dictionary for jsonifying
     entries_dict = {'entries': collect_entries(request.user)}
