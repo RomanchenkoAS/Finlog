95748266a829c1fb388fe7921e08959abaccf26d
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-02 18:41:05 +0600

    auto commit

--- log/urls.py
+++ log/urls.py
@@ -4,7 +4,7 @@ from . import views
 appname = 'log'
 urlpatterns = [
     path('', views.log, name='log'),
-    # AJAX requests path
+    # AJAX requests pathes
     path('add', views.add, name='add'),
     path('remove/<int:p>/', views.remove, name='remove'),
     path('load_content', views.load_content, name='load_content'),
