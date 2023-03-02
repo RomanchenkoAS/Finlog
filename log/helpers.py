5f6000018ceb3e78a24ac84445a7ac164038df6d
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-02 13:02:27 +0600

    renamed iterataor

--- log/helpers.py
+++ log/helpers.py
@@ -17,7 +17,7 @@ def collect_entries(user):
             'category': item.category.name,
             'datetime': item.date.strftime("%Y-%m-%d %H:%M:%S %Z"),
             'comment' : item.comment,
-            'iterator': i + 1,
+            'position': i + 1,
             'id'      : item.id,
         }
         # print(f'Adding an item #{i}: {item.value} / {item.category} / {item.date} || id:{item.id}')
