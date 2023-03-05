c8a13166b3f57adf352174e69fda76e11194681b
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-05 23:30:33 +0600

    urls upd

--- log/urls.py
+++ log/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # AJAX requests pathes
     path('add', views.add, name='add'),
     path('remove/<int:p>/', views.remove, name='remove'),
+    # A support path with no view || for loading list of Entries to the page 
     path('load_content/', views.load_content, name='load_content'),
 ]
