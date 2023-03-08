253b6d678bd9fefdf30e4153dfa8069f0bb23799
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-08 20:11:13 +0600

    new user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from log.models import Category
 
-# Create your models here.
+# Custom user model containing a list of categories and according methods
+class User(AbstractUser):
+    categories = models.ManyToManyField('Category', blank=True)
+    
+    def generate_categories(self):
+        # Add all the default categories to the user
+        list = Category.objects.all()
+        self.categories.add(list)
+
+    def get_categories(self):
+        return self.categories.all()
