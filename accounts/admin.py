d5fe138373124a0805e82f6b5461fd42a1d67264
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-03-09 15:55:23 +0600

    removed database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from .models import User, UserCategory
 # Register your models here.
 
 class MyUserAdmin(UserAdmin):
+    '''To monitor user's password info'''
     list_display = ('username', 'email', 'password_info')
 
     def password_info(self, obj):
         return f"Salt: {obj.password.split('$')[2]}, Hash: {obj.password.split('$')[-1]}"
     password_info.short_description = 'Password Info'
 
-admin.site.unregister(User)
+# admin.site.unregister(User)
 admin.site.register(User, MyUserAdmin)
+
+admin.site.register(UserCategory)
